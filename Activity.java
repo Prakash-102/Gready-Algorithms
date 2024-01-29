6d6595a747c9f70529cb077dcdaf4655f05eb5e0
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-29 11:50:59 +0530

    Add files via upload

---
+++ Activity.java
@@ -0,0 +1,47 @@
+
+//Activity selection:-M,G,A,F,F
+import java.util.*;
+
+public class Activity {
+
+    public static void main(String[] args) {
+        int start[] = { 1, 3, 0, 5, 8, 5 };
+        int end[] = { 2, 4, 6, 7, 9, 9 };
+        // Sorting
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+        // Lamda function -> shortform
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
+        // end time basis sorted
+        int maxAct = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        // 1st activity
+        maxAct = 1;
+        ans.add(activities[0][0]);
+        int LastEnd = activities[0][2];
+        for (int i = 1; i < end.length; i++) {
+            if (activities[i][1] >= LastEnd) {
+                // active select
+                maxAct++;
+                ans.add(activities[i][0]);
+                LastEnd = activities[i][2];
+            }
+        }
+        System.out.println("max activities = " + maxAct);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print("A" + ans.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
+
+/*
+ * Output:-
+ * max activities = 4
+ * A0 A1 A3 A4
+ */
